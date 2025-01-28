497ae6f3ec3a7f0b6c662f76ecbe5312cd82e0f5
javier <javier@formatinternet.com>
2025-01-28 09:56:23 +0100

    adding bash example

---
+++ bash/drop_partitions_older_than_21_days.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# This script needs both curl and jq installed.
+# It will go over all the tables with daily partitioning and will remove all partitions older than 21 days 
+# It uses jq to parse the JSON output from the REST API, extracting the "dataset" element and flatten all the rows.
+# Then it reads line by line and calls the QuestDB API with each ALTER TABLE statement.
+
+
+# We get all the tables with daily partitioning and compose the ALTER TABLE statements
+
+TABLES=`curl -G  --data-urlencode "query=with daily_tables AS (
+select name, designatedTimestamp, timestamp_floor('d',dateadd('d', -21, systimestamp())) as deadline from tables where partitionBy = 'DAY'
+)
+select CONCAT('ALTER TABLE ', name, ' DROP PARTITION WHERE ', designatedTimestamp, ' <= ', deadline) FROM daily_tables;" "http://localhost:9000/exec?nm=true"|jq ".dataset | flatten[]"`
+
+
+# Splitting the output line by line and issuing the ALTER TABLE
+printf '%s\n' "$TABLES" |
+while IFS= read -r sql; do
+        # echo $sql #uncomment if you want to output each statement we are sending
+        #we need to remove starting and trailing double quote from the line, so using :1:-1 syntax
+        curl -G --data-urlencode "query=${sql:1:-1}" http://localhost:9000/exec
+done
+
